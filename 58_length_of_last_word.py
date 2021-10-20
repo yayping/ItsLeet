c4feb7408049f500d1579c552f552999f03afcdf
yaping yang <yapingy5@gmail.com>
2021-10-19 22:41:11 -0400

    58 length of last word

---
+++ 58_length_of_last_word.py
@@ -0,0 +1,40 @@
+"""
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+"""
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count = 0
+        
+        for i in range(len(s)-1, -1, -1):
+            
+            if s[i] != ' ':
+                count += 1
+                
+            elif count > 0:
+                return count
+        return count
